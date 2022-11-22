23ff90a06a7838084b10127f2c4c26f01c7c40d8
Svetlio218 <118718784+Svetlio218@users.noreply.github.com>
2022-11-22 08:11:16 +0200

    Add files via upload

---
+++ app.js
@@ -0,0 +1,9 @@
+let pictures=["1.jpg","3.jpg","4.jpg","5.jpg"]
+let description=[
+    "description for pictore 1",
+    "description for pictore 3",
+    "description for pictore 4",
+    "description for pictore 5"
+]
+let picture=document.getElementById("pic");
+let description=document.getElementById("desc");
